9ce75b341b5607e180c2e1f0db4de1d9c3f042e2
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2023-10-03 08:41:52 -0400

    feature: tailwind prettier plugin installed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="flex justify-center items-center min-h-screen">
-    <p class="text-7xl font-bold text-center">Welcome!</p>
+  <div class="flex min-h-screen items-center justify-center">
+    <p class="text-center text-7xl font-bold text-red-200">Welcome!</p>
   </div>
 </template>
