4b3f88dc476d8f96268e05350abc415896521c89
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2023-10-03 10:20:41 -0400

    feat: home view implemented

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
 <script setup></script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center">
-    <p class="text-center text-7xl font-bold text-red-200">Welcome!</p>
+  <div class="container min-h-screen">
+    <img class="mb-20" src="@/assets/img/hero.jpg" alt="hero-poster" />
+    <div class="mb-20">
+      <h2 class="mb-4 text-4xl font-bold">Now in theaters</h2>
+      <div class="grid grid-cols-5 gap-5">
+        <img src="@/assets/img/nr_1.jpg" alt="" />
+        <img src="@/assets/img/nr_2.jpg" alt="" />
+        <img src="@/assets/img/nr_3.jpg" alt="" />
+        <img src="@/assets/img/nr_4.jpg" alt="" />
+        <img src="@/assets/img/nr_5.jpg" alt="" />
+      </div>
+    </div>
+    <div class="mb-8">
+      <h2 class="mb-4 text-4xl font-bold">Upcoming movies</h2>
+      <div class="grid grid-cols-5 gap-5">
+        <img src="@/assets/img/nr_1.jpg" alt="" />
+        <img src="@/assets/img/nr_2.jpg" alt="" />
+        <img src="@/assets/img/nr_3.jpg" alt="" />
+        <img src="@/assets/img/nr_4.jpg" alt="" />
+        <img src="@/assets/img/nr_5.jpg" alt="" />
+      </div>
+    </div>
   </div>
 </template>
