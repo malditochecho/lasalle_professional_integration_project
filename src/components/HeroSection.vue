e46080703a2fca0342f36d633392c2543d7bbbde
sergio <sergio_alberto.rpdriguez_ortiz@lcieducation.net>
2023-10-23 00:11:24 -0400

    refactor: hero section moved to a component so the image can be dynamic in the future

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('Elevate Your Movie Nights with Our Expert Recommendations!')
+const image = ref('src/assets/img/hero.jpg')
+</script>
+
+<template>
+  <div class="relative col-span-12">
+    <img :src="image" alt="hero-poster" class="grayscale" />
+    <p
+      class="absolute left-1/2 top-1/2 w-full -translate-x-1/2 -translate-y-1/2 bg-gray-200 p-4 text-center text-[1.5rem] font-bold text-black mix-blend-exclusion drop-shadow-2xl sm:text-[3rem] md:text-[4rem] lg:text-[5rem]"
+    >
+      {{ text }}
+    </p>
+  </div>
+</template>
